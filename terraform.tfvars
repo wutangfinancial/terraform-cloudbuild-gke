e24248f8dfd99c421daf8afc10e7329f65d25439
wutangfinancial <wutangfinancial@detwa.com>
2021-07-13 02:20:41 +0000

    add terraform files and tf plan step

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+project_id = "pipeline-319200"
+region     = "us-central1"
+zone     = "us-central1-a"
---
+++ vpc.tf
@@ -0,0 +1,18 @@
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "${var.project_id}-vpc"
+  auto_create_subnetworks = "false"
+}
